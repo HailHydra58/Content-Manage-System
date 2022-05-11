b26e5267433a896eb6aae48003c6dcfbbb6de209
HailHydra58 <604442841@qq.com>
2022-05-11 19:47:33 +0800

    完善打包的目录,还有换成hash模式

--- dessert/src/router/index.js
+++ dessert/src/router/index.js
@@ -1,4 +1,8 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {
+  createRouter,
+  createWebHistory,
+  createWebHashHistory,
+} from "vue-router";
 import store from "../store/index.js";
 
 import Login from "../views/login/Login.vue";
@@ -43,7 +47,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
